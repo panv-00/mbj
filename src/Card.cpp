6feae0c1c99a56eacf7d1b163a0d6e419f586406
panv-00 <panv@tutanota.com>
2022-07-05 22:53:18 +0100

    new file:   src/Card.cpp 	new file:   src/Card.h 	new file:   src/Pile.cpp 	new file:   src/Pile.h

---
+++ src/Card.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#include "Card.h"
+
+Card::Card()
+{
+
+}
+
+Card::~Card()
+{
+
+}
+
+int Card::find(const char arr[], int len, char seek)
+{
+  for (int i = 0; i < len; ++i)
+  {
+    if (arr[i] == seek) return i;
+  }
+  return -1;
+}
+
+int Card::face_to_value(char f)
+{
+  int face_index = find(card_faces, 13, f);
+
+  return card_values[face_index];
+}
+
+void Card::set_face_suit(char f, char s)
+{
+  face = f;
+  suit = s;
+  value = face_to_value(f);
+}
+
+void Card::dump()
+{
+  printf("%c%c, ", face, suit);
+}
+
+void Card::set_card(Card card)
+{
+  set_face_suit(card.get_face(), card.get_suit());
+}
